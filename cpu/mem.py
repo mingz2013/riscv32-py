230e0894304f06ab48449f7529107ff43852a674
mingz <305603665@qq.com>
2020-01-27 13:19:09 +0800

    riscv32

--- cpu/mem.py
+++ cpu/mem.py
@@ -8,7 +8,7 @@ Module Description
 
 """
 
-from memory.memory import Memory
+import struct
 
 
 class MEM(object):
@@ -18,15 +18,86 @@ class MEM(object):
     """
 
     def __init__(self, cpu):
-        self.cpu = cpu
-        self.memory = Memory()
-        self.memory.load_bin()
+        # self.cpu = cpu
+        self.memory = None
+        self.load_bin()
+
+    @property
+    def max_len(self):
+        return len(self.memory)
+
+    def load_bin(self):
+        with open('./hello/hello.bin', 'rb') as f:
+            # index = 5
+            # while True:
+            #     index -= 1
+            #     if index < 0:
+            #         break
+            #     b = f.read(4)
+            #     print(type(b), len(b))
+            #     i, = struct.unpack('<i', b)
+            #     print(bin(i), hex(i))
+            #     self.memory.append(b)
+            self.memory = f.read()
+            # self.max_len = len(self.memory)
+            print("load bin: ", "len: ", self.max_len)
 
     def load_instruction(self, pc):
-        """
-
-        """
-        return self.memory.load_instruction(pc)
+        print("load_instruction pc: ", pc)
+        if pc > self.max_len:
+            raise Exception(pc, self.max_len)
+        b = self.memory[pc:pc + 4]
+        i, = struct.unpack('<i', b)
+        print(bin(i), hex(i))
+        return i
 
     def read_byte(self, pc, len=1):
-        return self.memory.read_byte(pc, len=len)
+        print("read_byte pc: ", pc, "len: ", len)
+        if pc > self.max_len:
+            raise Exception(pc, self.max_len)
+        b = self.memory[pc:pc + len]
+        print("b: ", b)
+        i, = struct.unpack('<i', b)
+        print(bin(i), hex(i))
+        return i
+
+    def write_byte(self, addr, b):
+        print('write_byte: ', addr, b)
+        if addr > self.max_len:
+            raise Exception(addr, self.max_len)
+        for i in range(len(b)):
+            self.memory[addr + i] = b[i]
+
+    def read_file(self):
+        with open('./hello/hello.bin', 'rb') as f:
+            index = 20
+            count = 0
+            while True:
+                # while index > 0:
+                index -= 1
+                b = f.read(4)
+                # print("b:", b)
+                if len(b) == 0:
+                    print("over....b == 0")
+                    break
+                # print(type(b), len(b))
+                i, = struct.unpack('<i', b)
+                print(i, type(i), bin(i), hex(i))
+                # print(type(bin(i)))
+                if i == 0:
+                    break
+                # parse(i)
+                count += 1
+                print("count ...", count)
+
+
+if __name__ == '__main__':
+    memory = MEM(None)
+    m = memory
+    m.load_bin()
+
+    print(m.memory)
+    print(type(m.memory))
+    # pc = 0x0000004c
+    pc = 0x1c  # offset = 0x10090 - 0x10074, 根据汇编代码的偏移，计算偏移，copy出代码段后，用0+offset = 0x1c， eip
+    i = m.load_instruction(pc)
--- memory/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@FileName: __init__.py
-@Time: 2020/1/25 09:46
-@Author: zhaojm
-
-Module Description
-
-"""
